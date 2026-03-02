981e6b64fbff9196a1fd662f31cfad27010fa90a
xglaxx <karla68santos@gmail.com>
2026-03-02 01:49:24 -0300

    Args

--- src/ports/ConfigMetadata.js
+++ src/ports/ConfigMetadata.js
@@ -9,16 +9,16 @@ export default class ConfigMetadata extends SetMetadata {
    _set(data) {
       if (typeof data !== "object") throw data;
       
-      this.dir = data?.dir;
-      this.path = data?.path;
-      this.scale = data?.scale;
-      this.chars = data?.chars;
-      this.output = data?.output;
-      this.fontDir = data?.fontDir;
-      this.fontSize = data?.fontSize;
-      this.colorFont = data?.colorFont;
-      this.colorChars = data?.colorChars;
-      this.colorBackground = data?.colorBackground;
+      this.dir = (data?.dir || "");
+      this.path = (data?.path || "");
+      this.scale = (data?.scale || 0);
+      this.chars = (data?.chars || "");
+      this.output = (data?.output || "");
+      this.fontDir = (data?.fontDir || "");
+      this.fontSize = (data?.fontSize || 0);
+      this.colorFont = (data?.colorFont || "");
+      this.colorChars = (data?.colorChars || "");
+      this.colorBackground = (data?.colorBackground || "");
       this.changeFont(data?.font, data?.nameFont);
    }
 }
